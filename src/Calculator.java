ff73c3403ca08fc1036e85fcf1d216562f879a24
Bik237 <ngantybikele@gmail.com>
2025-05-05 18:05:26 +0100

    fourth commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Arrays;
-import java.util.Arrays.*;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
@@ -29,8 +28,13 @@ public class Calculator {
     JPanel displayPanel = new JPanel();
     JPanel buttonPanel = new JPanel();
 
+    // A+B, A-B, A/B
+    String A = "0";
+    String operator = null;
+    String B = null;
+
     Calculator() {
-        frame.setVisible(true);
+        // frame.setVisible(true);
         frame.setSize(boardwidth, boardheight);
         frame.setLocationRelativeTo(null);// mettre le cadre au centre
         frame.setResizable(false);
@@ -50,7 +54,7 @@ public class Calculator {
 
         buttonPanel.setLayout(new GridLayout(5, 4));// nouvelle disposition de grid 5 lignes 4 colonnes
         buttonPanel.setBackground(black);
-        frame.add(buttonPanel);
+        frame.add(buttonPanel);// ajoute buttonPanel dans mon objet frame
 
         for (int i = 0; i < buttonValues.length; i++) {
             JButton button = new JButton();
@@ -63,13 +67,16 @@ public class Calculator {
             if (Arrays.asList(topSymbols).contains(buttonvalue)) {
                 button.setBackground(gray);
                 button.setForeground(Color.black);// couleur du texte en blanc
+
             } else if (Arrays.asList(rightSymbols).contains(buttonvalue)) {
+
                 button.setBackground(orange);
                 button.setForeground(Color.white);
             } else {
                 button.setBackground(dark);
                 button.setForeground(Color.white);
             }
+
             buttonPanel.add(button);
 
             button.addActionListener(new ActionListener() {
@@ -82,15 +89,73 @@ public class Calculator {
                     // Arrays.aslist converti le tableau rightSymbols en liste
                     if (Arrays.asList(rightSymbols).contains(buttonvalue)) {
 
+                        if (buttonvalue == "=") {
+                            if (A != null) {
+
+                                B = displayLabel.getText();
+                                double numA = Double.parseDouble(A);
+                                double numB = Double.parseDouble(B);
+
+                                if (operator == "+") {
+                                    displayLabel.setText(removeZeroDecimal(numA + numB));
+                                } else if (operator == "-") {
+                                    displayLabel.setText(removeZeroDecimal(numA - numB));
+                                } else if (operator == "*") {
+                                    displayLabel.setText(removeZeroDecimal(numA * numB));
+                                } else if (operator == "/") {
+                                    displayLabel.setText(removeZeroDecimal(numA / numB));
+                                }
+                                clearAll();
+                            }
+
+                        } else if ("+-*/".contains(buttonvalue)) {
+                            if (operator == null) {// s'exécute seulement si aucun opérateur n'était déjà sélectionné.
+                                A = displayLabel.getText();
+                                displayLabel.setText("0");// Réinitialise l'affichage à "0" pour la saisie du second
+                                                          // nombre.
+                                B = "0";// Initialise le second opérande B à "0"
+                            }
+
+                            operator = buttonvalue;// Cette ligne s'exécute toujours, que operator soit
+                            // null ou pas.
+                        }
+
                     } else if (Arrays.asList(topSymbols).contains(buttonvalue)) {
+
                         // si buttonvalue est dans le tableau topSymbole
+                        if (buttonvalue == "AC") {
+                            clearAll();
+                            displayLabel.setText("0");// redefini le texte a "0"
+
+                        } else if (buttonvalue == "+/-") {
+
+                            double numDisplay = Double.parseDouble(displayLabel.getText());// convertie le nombre en
+                                                                                           // double
+                            numDisplay *= -1;// multiplie par -1 inverse le signe
+                            displayLabel.setText(removeZeroDecimal(numDisplay));// Met à jour l'affichage avec le
+                                                                                // nouveau nombre.
+
+                        } else if (buttonvalue == "%") {
+
+                            double numDisplay = Double.parseDouble(displayLabel.getText());// convertie le nombre en
+                                                                                           // double
+                            numDisplay /= 100;// divise par 100
+                            displayLabel.setText(removeZeroDecimal(numDisplay));// Met à jour l'affichage avec le
+                                                                                // nouveau nombre
+                        }
+
                     } else {
                         if (buttonvalue == ".") {
+                            if (!displayLabel.getText().contains(buttonvalue)) {
+                                displayLabel.setText(displayLabel.getText() + buttonvalue);
+                            }
 
                         } else if ("0123456789".contains(buttonvalue)) {
+
                             // verifie si buttonvalue est l'un des caracteres de 0-9
                             if (displayLabel.getText().equals("0")) {
                                 displayLabel.setText(buttonvalue);// remplace "0" par la valeur du button appuyé
+
                             } else {
                                 displayLabel.setText(displayLabel.getText() + buttonvalue);
                                 // Si displayLabel affiche "12" et qu'on appuie sur "3" → "123" permet de
@@ -100,7 +165,29 @@ public class Calculator {
                         }
                     }
                 }
+
             });
+            frame.setVisible(true);
         }
+
     }
+
+    void clearAll() {
+        // met la variable A a "0", les operateurs et la variable B a "0"
+        // 11+2 11=A operateur=+ 2=B
+        A = "0";
+        operator = null;
+        B = null;
+    }
+
+    String removeZeroDecimal(double numDisplay) {
+        // Convertir un nombre en String en supprimant la partie décimale si elle est
+        // nulle (ex: 5.0 → "5").
+        if (numDisplay % 1 == 0) {
+            return Integer.toString((int) numDisplay);// Convertit le double en int (via (int) numDisplay). Retourne la
+                                                      // version sans .0 (Integer.toString()).
+        }
+        return Double.toString(numDisplay);// Retourne le nombre décimal tel quel (Double.toString()).
+    }
+
 }
